0a9dafceb913b4d65c26558a5c3f33423141cd48
DR_GURUNG <40120998+racingfoxs@users.noreply.github.com>
2023-04-11 12:38:39 +0545

    domain setup 2

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <Provider store={store}>
-    <BrowserRouter basename="/bm-new">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </Provider>
